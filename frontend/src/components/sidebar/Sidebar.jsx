a5d32d4cfae6b6442f42e2686a8cb2e26617c672
ひろ <hiroyuki09171016@gmail.com>
2023-06-27 21:35:40 +0900

    とりあえず完成

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,39 @@
+import { Home, Person, Settings } from '@mui/icons-material'
+import React from 'react'
+import "./Sidebar.css";
+import CloseFriend from '../closeFriend/CloseFriend';
+import { Users } from '../../dummyData'
+import { Link } from 'react-router-dom';
+
+export default function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <div className="sidebarWrapper">
+                <ul className='sidebarList'>
+                    <li className='sidebarListItem'>
+                        <Home className='sidebarIcon' />
+                        <Link to="/" style={{ textDecoration: "none", color: "black" }} >
+                            <span className='sidebarListItemText'>ホーム</span>
+                        </Link>
+                    </li>
+                    <li className='sidebarListItem'>
+                        <Person className='sidebarIcon' />
+                        <Link to="/profile/Hiro/" style={{ textDecoration: "none", color: "black" }}>
+                            <span className='sidebarListItemText'>プロフィール</span>
+                        </Link>
+                    </li>
+                    <li className='sidebarListItem'>
+                        <Settings className='sidebarIcon' />
+                        <span className='sidebarListItemText'>設定</span>
+                    </li>
+                </ul>
+                <hr className='sidebarHr' />
+                <ul className='sidebarFriendList'>
+                    {Users.map((user) => (
+                        <CloseFriend user={user} key={user.id} />
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/components/topbar/Topbar.jsx
@@ -0,0 +1,34 @@
+import { Chat, Notifications } from '@mui/icons-material'
+import React, { useContext } from 'react'
+import "./Topbar.css"
+import { Link } from 'react-router-dom'
+import { AuthContext } from "../../state/AuthContext";
+
+export default function Topbar() {
+    const { user } = useContext(AuthContext);
+    const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
+    return (
+        <div className='topbarContainer'>
+            <div className="topbarLeft">
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <span className='logo'>DAYCE SNS</span>
+                </Link>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarIconItems">
+                    <div className="topbarIconItem">
+                        <Chat />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Notifications />
+                        <span className="topbarIconBadge">2</span>
+                    </div>
+                    <Link to={`/profile/${user.username}`}>
+                        <img src={user.profilePicture ? PUBLIC_FOLDER + user.profilePicture : PUBLIC_FOLDER + "/person/noAvatar.png"} alt="" className='topbarimg' />
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
