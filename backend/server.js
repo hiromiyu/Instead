df5cef9f5628ca6ec57baa3440bf080bd465f159
ひろ <hiroyuki09171016@gmail.com>
2023-08-04 21:16:47 +0900

    ポートの変更

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const uploadRoute = require("./routes/upload");
-const PORT = 4000;
-// const PORT = 443;
+// const PORT = 4000;
+const PORT = 443;
 const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config();
@@ -44,9 +44,9 @@ app.use("/api/upload", uploadRoute);
 //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World!");
-// })
+app.get("/", (res) => {
+    res.json("Hello World!");
+})
 // app.get("/users", (req, res) => {
 //     res.send("users World!");
 // })
