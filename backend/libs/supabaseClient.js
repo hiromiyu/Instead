ffdd7eb34bcaf6c353c07e733d38208e2e0ba773
ひろ <hiroyuki09171016@gmail.com>
2023-08-23 23:49:09 +0900

    環境変数名の変更

--- backend/libs/supabaseClient.js
+++ backend/libs/supabaseClient.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = process.env.NODE_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NODE_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.NODE_PUBLIC_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Missing Supabase URL or anon key");
