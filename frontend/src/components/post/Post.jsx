9c29597ccac4f46eabe89d81c77426a5f8e45ca4
ひろ <hiroyuki09171016@gmail.com>
2023-08-25 22:55:51 +0900

    いいねボタンを少し小さく いいね数を押せるように

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -116,6 +116,7 @@ export default function Post({ post }) {
                             />
                             <span className="postLikeCounter"
                                 style={{ color: isLiked ? "red" : "gray" }}
+                                onClick={() => handleLike()}
                             >
                                 {like}
                             </span>
