0be2f97d2a56631f79aad932ed8fe4e3f785e2a3
ひろ <hiroyuki09171016@gmail.com>
2025-08-31 22:56:53 +0900

    sameSite: 'none'に変更

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -57,7 +57,7 @@ router.post('/login', async (req, res) => {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: 'Strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
